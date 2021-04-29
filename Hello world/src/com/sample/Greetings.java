2e1db10269158e66257742f8732b033b3846a076
890165 <890165@PC426969.cts.com>
2021-04-29 10:24:36 +0530

    Initial commit

---
+++ Hello world/src/com/sample/Greetings.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+public class Greetings 
+
+{
+public void WelcomeNote()
+{
+	System.out.println("A wonderful Welcome to all gathered here. The meeting will started in 5 minutes.");
+}
+public void Contents()
+{
+	System.out.println("1.Welcome song");
+	System.out.println("2.Jury Introduction");
+	System.out.println("3.Jury Speech");
+	System.out.println("4.Prizes distribution");
+	System.out.println("5.General Speeches");
+}
+public void closingNote()
+{
+	System.out.println("Thank you all for your presence");
+}
+}
---
+++ Hello world/src/com/sample/Main.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	
+	{
+		// TODO Auto-generated method stub
+    Greetings g=new Greetings();
+    g.WelcomeNote();
+    g.Contents();
+    g.closingNote();
+	}
+
+}
