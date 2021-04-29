f6f8d247ac95c7f60ab2f7155031dcee3cf47bee
890165 <890165@PC426969.cts.com>
2021-04-29 11:38:49 +0530

    Initial Commit

---
+++ Loops/src/com/sample/Ifloop.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class Ifloop 
+{
+String c;
+void gender()
+{
+if(c=="Female")
+{
+	System.out.println("Your gender is female");
+}
+else if(c=="Male")
+{
+	System.out.println("Your gender is male");
+}
+}
+}
---
+++ Loops/src/com/sample/Main.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		Ifloop i=new Ifloop();
+		i.c="Female";
+		i.gender();
+
+	}
+
+}
