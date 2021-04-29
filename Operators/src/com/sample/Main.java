0adb21b5f746326eb58e0168a9fa5a42c890e2ff
890165 <890165@PC426969.cts.com>
2021-04-29 11:18:18 +0530

    Initial Commit

---
+++ Operators/src/com/sample/Main.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+    add a=new add();
+    System.out.println("The sum is "+a.add(10, 20));
+	}
+
+}
---
+++ Operators/src/com/sample/add.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+public class add 
+{
+public int add(int a,int b)
+{
+	int sum=a+b;
+	return sum;
+}
+}
